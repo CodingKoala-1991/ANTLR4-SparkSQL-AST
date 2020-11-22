80359df73b7149df6e7b05382fbcb88232130578
CodingKoala-1991 <alancjchen1991@gmail.com>
2020-11-22 21:29:46 +0800

    xx

--- src/main/java/org/codingkoala/antlr4_spark_sql_ast/ParserDriver.java
+++ src/main/java/org/codingkoala/antlr4_spark_sql_ast/ParserDriver.java
@@ -3,6 +3,8 @@ package org.codingkoala.antlr4_spark_sql_ast;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+import java.util.Arrays;
+
 public class ParserDriver {
     public static void main(String[] args) {
         // 关于 language level 相关的问题
@@ -10,8 +12,6 @@ public class ParserDriver {
         // 如果设置成5， 在 gen 下一大堆 @Override 的 代码会飘红，虽然能运行，设置成 6 就不会飘红
         // 同时还要在 Preferences -> Build, Execution, Deployment -> Compiler -> Java Compiler 里
         // 把 target bytecode version 改成 6，不然会说 Java Compiler 有问题之类的，运行不起来
-
-        // 将 maven 的 settings 设置成国内镜像
 
 
         // String query = "(select a, b, c from tables";
@@ -26,9 +26,13 @@ public class ParserDriver {
         System.out.println(String.format("The query is %s", query));
         System.out.println("********************************************");
         SqlBaseLexer lexer = new SqlBaseLexer(new ANTLRInputStream(query.toUpperCase()));
-        SqlBaseParser parser = new SqlBaseParser(new CommonTokenStream(lexer));
-        MyVisitor visitor = new MyVisitor();
-        String res = visitor.visitSingleStatement(parser.singleStatement());
-        System.out.println("res="+res);
+
+//        String text = lexer.getToken();
+//        System.out.println(text);
+////        SqlBaseParser parser = new SqlBaseParser(new CommonTokenStream(lexer));
+//        MyVisitor visitor = new MyVisitor();
+//        // SqlBaseBaseVisitor visitor = new SqlBaseBaseVisitor();
+//        String res = visitor.visitSingleStatement(parser.singleStatement());
+//        System.out.println("res="+res);
     }
 }
