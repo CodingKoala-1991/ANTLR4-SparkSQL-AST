fd61e75ead858a4bef4c81dc60a04323c91abe2d
CodingKoala-1991 <alancjchen1991@gmail.com>
2021-03-15 23:48:08 +0800

    tmp

--- src/main/java/org/codingkoala/antlr4_spark_sql_ast/ParserDriver.java
+++ src/main/java/org/codingkoala/antlr4_spark_sql_ast/ParserDriver.java
@@ -16,7 +16,7 @@ public class ParserDriver {
 
         // String query = "(select a, b, c from tables";
         // String query = "select * from xxx where yy = 'z'";
-        String defaultQuery = "select name from student where age > 18 order by id desc";
+        String defaultQuery = "select name from student where age > 18 order by id, name";
         String query;
         if (args.length > 0) {
             query = args[0];
@@ -35,14 +35,21 @@ public class ParserDriver {
 
         // 在 g4 文件中
         // 小写 xyz 是一个语法规则
+
+        // *********** 关于 visitor ***********
         // 如果 xyz 语法规则 不带有 分支#，就会在 visitor 中生成 visitXyz() 方法
         // 如果 xyz 语法规则 带有 分支#，每一个分支 都会在 visitor 中生成 visitXxx() 方法
 
-        // 每个 xyz 语法规则 会在 语法解释器parser 中生成一个 xyz() 方法
+        // *********** 关于 context ***********
         // xyz 语法规则 和 他的分支（不管有没有），都会在 语法解释器parser 中生成 XxxContext
         // 分支的 context 会 extends 语法规则的 context
-        // 在这里，语法解释器parser 中的 SingleStatementContext 就是传入的 query 生成的 AST 的 root 节点
+        // 例如，语法解释器parser 中的 SingleStatementContext 就是传入的 query 生成的 AST 的 root 节点
         // parser.singleStatement() 返回的就是 SingleStatementContext 的对象
+        // context 内部通过
+
+        // *********** 关于 context 对应的访问方法 ***********
+        // 每个 xyz 语法规则 会在 语法解释器parser 中生成一个 xyz() 方法
+        // 在构建 AST的过程中，context 内部调用这些不同的 xyz() 方法，可以生成不同子 context
 
         // 在 语法解释器parser 中的每一个 Context 里
         // 如果 xyz语法规则 不包含分支，那么会在 context 里重构 enterRule exitRule accept 和 getRuleIndex 4个方法
@@ -51,7 +58,15 @@ public class ParserDriver {
         // enterRule exitRule 是用于 listener 模式，所以在这里不用管
         // accept 用于 visitor 模式
 
-        SqlBaseParser.SingleStatementContext  singleStatementContext = parser.singleStatement();
+        // 有两个 xyZ() 方法，分别位于 parser 里 和 context 里？？？？？？？
+        // 构建 AST 的调用样例？？？？？？
+        // visitor 访问的调用样例 ？？？？？
+        // += 的列表 和 普通的 正则列表不一样，前者需要记住一些信息？？？？？？？？
+        // 一个规则里，如果同一个 子语法规则 或者 词法规则有多个，那么会生成一个 list
+
+        // parser.singleStatement() 构建了一棵 Context 树（AST），每一个节点都是一个 context，并最终返回 root 的 context
+        // 这个 root context 最终交给 visitor，去访问这个 AST
+        SqlBaseParser.SingleStatementContext singleStatementContext = parser.singleStatement();
 
         System.out.println("****************************");
         visitor.visitSingleStatement(singleStatementContext);
